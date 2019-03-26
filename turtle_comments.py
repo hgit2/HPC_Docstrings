1d49f6d3ed7c4979fa4d200ceefcee48b095c5b1
Alice GENESTIER <alice.genestier@gmail.com>
2019-03-26 18:49:42 +0100

    ajout commentaires trois premières fonctions

--- turtle_comments.py
+++ turtle_comments.py
@@ -1,19 +1,43 @@
+
 import turtle
+
 def carre(animal,size):
+    """Args:
+        animal (str): name of the animal, must be one word without spaces.
+        size (int): size of the square, must be an integer.
+       
+       Return:
+        draw a square of size "size", the drawing made by the chosen animal.
+        """
     for i in range (4):
         animal.forward(size)
         animal.left(90)
-#1
-"""def petitscarres(animal,n,size):
+
+def petitscarres(animal,n,size):
+    """Args:
+        animal (str): name of the animal, must be one word without spaces.
+        n (int) : number of square to draw, must be an integer.
+        size (int): size of the square, must be an integer.
+       
+       Return:
+        draw n square of size "size", the drawing made by the chosen animal.
+        """
     a=0
     for i in range (n):
         carre(animal,size)
         animal.penup()
         animal.forward(40)
-        animal.pendown()"""
+        animal.pendown()
 
-#2
-"""def carresconc(animal,n,size):
+def carresconc(animal,n,size):
+    """Args:
+        animal (str): name of the animal, must be one word without spaces.
+        n (int) : number of square to draw, must be an integer.
+        size (int): size of the first square (the smallest) , must be an integer.
+       
+       Return:
+        draw n concentric squares of initial size "size", the drawing made by the chosen animal.
+        """
     a=size
     for i in range(n):
         carre(animal,size)
@@ -23,23 +47,23 @@ def carre(animal,size):
         animal.left(90)
         animal.forward(-a/2)
         size=size+20
-        animal.pendown()"""
-#3
-"""def draw_poly(t,n,sz):
+        animal.pendown()
+
+def draw_poly(t,n,sz):
     for i in range(n):
         t.forward(sz)
-        t.left(360/n)"""
-#4
-"""def bigstar(t,n,sz):
+        t.left(360/n)
+
+def bigstar(t,n,sz):
     for i in range (n):
         for k in range (4):
             carre(t,sz)
             t.left(-90)
-        t.left(-90/n)"""
-#5
-"""def spiral(t,n,angle,size,space):
+        t.left(-90/n)
+
+def spiral(t,n,angle,size,space):
     t.left(-180)
     for i in range (n):
         t.forward(size)
         t.left(-90-angle)
-        size+=space"""
+        size+=space
