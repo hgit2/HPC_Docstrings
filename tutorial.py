9566ff0405dfc716d94bf3595e1f970fda15ce13
Hana LBATH <hana.lbath@insa-lyon.fr>
2019-04-04 09:56:28 +0200

    created a tutorial

---
+++ tutorial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##################################################################################################################################
+####################################################    turtle_comments.py  TUTORIAL #############################################
+##################################################################################################################################
+
+# General Imports
+import turtle 
+
+# turtle_comments.py Import
+import turtle_comments
+
+##################################################################################################################################
+
+##### First you need to create your animal, a turtle object that will draw your figures #####
+
+### Start by initializing a window
+window=turtle.Screen()
+
+### Then instantiate your animal (a lion here)
+lion=turtle.Turtle() 
+
+##### Then you can call the drawing function of your choice #####
+
+#print(spiral(tess,10,13, 20,9))
+#turtle.mainloop()
